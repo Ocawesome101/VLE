d3425a7cefe34ddfe87469cd313a5ec99a9bbcfe
Ocawesome101 <ocawesome101@gmail.com>
2020-12-31 02:17:18 -0500

    starting to take shape

--- base.lua
+++ base.lua
@@ -25,10 +25,18 @@ for i=1, #args, 1 do
   mkbuffer(args[i])
 end
 
-local function update_cursor()
-end
-
-local function draw_line()
+local function update_cursor(l)
+  local buf = buffers[current]
+  local line = buf.line
+  local scroll = buf.scroll
+  local from_end = buf.cursor
+  local text_len = #buf.lines[line]
+  local y = 0
+  for i=scroll, line - 1, 1 do
+    y = y + (l[i] or 0)
+  end
+  vt.set_cursor(1, y)
+  io.write(string.rep("\27[C", text_len - from_end))
 end
 
 -- status bar on bottom
@@ -37,7 +45,9 @@ local function redraw_buffer()
   vt.set_cursor(1, 1)
   local buf = buffers[current]
   local written = 0
+  local line_len = {}
   for i=1, h, 1 do
+    vt.set_cursor(1, i)
     local line = i + buf.scroll
     io.write("\27[2K")
     if buf.lines[line] then
@@ -45,6 +55,7 @@ local function redraw_buffer()
       if written + lines > h then
         break
       end
+      line_len[line] = lines
       written = written + lines
       io.write(buf.lines[line])
     else
@@ -61,16 +72,30 @@ local function redraw_buffer()
   end
   vt.set_cursor(w - 12, h)
   io.write(string.format("%d", buf.line))
-  update_cursor()
+  update_cursor(line_len)
+end
+
+local function process(key)
+  local buf = buffers[current]
+  local line = buf.line
+  local ltext = buf.lines[line]
+  local dfe = buf.cursor
+  if key == "backspace" then
+    buf.lines[line] = ltext:sub(1, #ltext - cursor - 1) .. ltext:sub(1, #ltext - cursor)
+  elseif key == "left" then
+  elseif key == "right" then
+  elseif key == "up" then
+  elseif key == "down" then
+  end
 end
 
 local function getcommand()
   vt.set_cursor(1, h)
-  io.write(":")
+  io.write("\27[2K:")
   local buf = ""
   while true do
     vt.set_cursor(2, h)
-    io.write(buf)
+    io.write(buf.." \27[D")
     local key, flags = kbd.get_key()
     flags = flags or {}
     if not (flags.ctrl or flags.alt) then
@@ -79,6 +104,8 @@ local function getcommand()
       else
         buf = buf .. key
       end
+    elseif flags.ctrl and key == "m" then
+      return buf
     end
   end
 end
@@ -105,10 +132,17 @@ while true do
   if flags.ctrl then
     if key == "i" then
       insert = not insert
+    elseif key == "h" and insert then
+      process("backspace")
     end
   elseif insert then
     process(key)
   elseif key == ":" then
-    getcommand()
+    local cmd = getcommand()
+    for k, v in pairs(commands) do
+      if cmd:match(k) then
+        v(cmd:match(k))
+      end
+    end
   end
 end
